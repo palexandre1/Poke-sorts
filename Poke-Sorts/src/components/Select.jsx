67a1a5020a05e4dbbabb6349b2e61eaa9f2c970b
Patrick Alexandre <palexandre822@gmail.com>
2023-10-28 11:03:12 -0600

    render pokemon on select screen with css

--- Poke-Sorts/src/components/Select.jsx
+++ Poke-Sorts/src/components/Select.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import {
   useQuery,
 } from '@tanstack/react-query'
-import { useLoaderData } from 'react-router-dom'
+import { useLoaderData, Link } from 'react-router-dom'
 import { getPokemon, getAllPokemon } from '../lib/data';
 
 const apiURL = 'https://pokeapi.co/api/v2/pokemon?limit=386';
 
 const loadPokemon = async (data) => {
-  const monData = await Promise.all(data.map(async pokemon => {
+  const monData = await Promise.all(data.map(async (pokemon) => {
     const pokemonGet = await getPokemon(pokemon);
     return pokemonGet;
   }));
@@ -42,9 +42,32 @@ function Select() {
   console.log(data);
 
   return (
-    <span>
-      {data.length !== undefined && data.length > 0 ? 'Data was successfully fetched' : 'Data not retrieved'}
-    </span>
+    <>
+      <div className="flex items-center">
+        <Link to="/">
+          <button type="button" className="flex sm:inline-flex justify-center items-center bg-gradient-to-tr from-pink-500 to-red-400 hover:from-pink-600 hover:to-red-500 active:from-pink-700 active:to-red-600 focus-visible:ring ring-pink-300 text-white font-semibold text-center rounded-md outline-none transition duration-100 px-5 py-2">Home</button>
+        </Link>
+      </div>
+      <div className="w-full relative group">
+        <div className="max-w-[1240px] mx-auto grid md:grid-cols-6 gap-8">
+          {data.map((pokemon) => (
+            <div
+              key={pokemon.id}
+              className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300"
+            >
+              <img
+                className="w-20 mx-auto mt-[-3rem] bg-white"
+                src={pokemon.sprites.front_default}
+                alt="/"
+              />
+              <h2 className="capitalize text-2xl font-bold text-center py-8">
+                {pokemon.name}
+              </h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
